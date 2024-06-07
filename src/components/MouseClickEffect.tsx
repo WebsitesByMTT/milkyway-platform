c4bad4d491ddb3f217fcc8bfac6c3521715b438a
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-06-07 10:47:43 +0530

    err: at cookies

--- src/components/MouseClickEffect.tsx
+++ src/components/MouseClickEffect.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 
 export default function MouseClickEffect() {
-  function handleClick(event) {
+  function handleClick(event: any) {
     const effect = document.createElement("div");
     effect.classList.add("click-effect");
     effect.style.left = `${event.clientX}px`;
