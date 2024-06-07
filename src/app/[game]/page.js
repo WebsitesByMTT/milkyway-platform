b37c13fbc4993f9cb9cd701f083ac4d26e67b905
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-06-07 18:20:53 +0530

    fix cookie 3

--- src/app/[game]/page.js
+++ src/app/[game]/page.js
@@ -1,21 +1,13 @@
 "use server";
 import GameIframe from "@/components/GameIFrame";
 import { redirect } from "next/navigation";
-
-async function getGameById(gameId) {
-  const res = await fetch(`http://localhost:3000/api/games/${gameId}`);
-
-  if (!res.ok) {
-    return undefined;
-  }
-  return res.json();
-}
+import { getGameById } from "../../utils/actions";
 
 const page = async ({ params }) => {
   const { game } = params;
   const data = await getGameById(game);
 
-  if (!data) {
+  if (data.game === null) {
     redirect("/");
   }
 
