c4ee8940304fcae69aaa57735b2d3270723824fc
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-06-07 17:51:25 +0530

    fix cookie 2

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,41 +1,16 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { config } from "./config";
+import jwt from "jsonwebtoken";
 
-async function getCookieData() {
-  const cookieData = cookies().get("token")?.value as string;
-  return cookieData;
-}
+export const getCookie = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+  return token;
+};
 
-export async function getUser() {
-  try {
-    const token = await getCookieData();
-
-    const response = await fetch(`${config.server}/api/users/userData`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `userToken=${token}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Fetch failed:", error);
-  }
-}
-
-export async function getGames() {
-  const res = await fetch("http://localhost:3000/api/games");
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-  return res.json();
-}
+export const getCurrentUser = async () => {
+  const token = await getCookie();
+  const user = jwt.decode(token as string);
+  return user;
+};
