60de15d826dc37302f57395f9b4944346c631c90
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-06-18 19:04:52 +0530

    fixed featured game click

--- src/components/FeaturedGameCard.jsx
+++ src/components/FeaturedGameCard.jsx
@@ -1,17 +1,20 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import { getGameById } from "@/utils/actions";
 
 const FeaturedGameCard = ({ src, setCurrentGame, setIsModalOpen }) => {
-  const gameOpenHandler = () => {
-    setCurrentGame(src?.gameHostLink);
+  const gameOpenHandler = async (id) => {
+    const url = await getGameById(id);
+    console.log("URL : ", url);
+    setCurrentGame(url.url);
     setIsModalOpen(true);
   };
   return (
     <div className="w-[20%] h-auto py-[3%] ">
       <div
         className="w-[90%] h-full rounded-[6%] p-[2%] bg-[#B18423] shadow-lg "
-        onClick={gameOpenHandler}
+        onClick={() => gameOpenHandler(src._id)}
       >
         <div className="bg-[#DC6E0E] rounded-[6%] w-full h-full p-[3%]">
           <div className="bg-gradient-to-b p-[2%] rounded-[6%] from-[#EFC54C] shadow-lg via-[#F98F08] to-[#943E00] w-full h-full">
