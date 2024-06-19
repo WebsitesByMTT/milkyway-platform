81d620d79c008b9bc6bdc1880c88905a4f4d8e9b
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-06-19 13:02:03 +0530

    add intersection observer

--- src/components/FeaturedGameCard.jsx
+++ src/components/FeaturedGameCard.jsx
@@ -1,21 +1,11 @@
-"use client";
 import Image from "next/image";
 import React from "react";
-import { getGameById } from "@/utils/actions";
+import Link from "next/link";
 
-const FeaturedGameCard = ({ src, setCurrentGame, setIsModalOpen }) => {
-  const gameOpenHandler = async (id) => {
-    const url = await getGameById(id);
-    console.log("URL : ", url);
-    setCurrentGame(url.url);
-    setIsModalOpen(true);
-  };
+const FeaturedGameCard = ({ src }) => {
   return (
-    <div className="w-[20%] h-auto py-[3%] ">
-      <div
-        className="w-[90%] h-full rounded-[6%] p-[2%] bg-[#B18423] shadow-lg "
-        onClick={() => gameOpenHandler(src._id)}
-      >
+    <Link href={`/${src._id}`} className="w-[20%] h-auto py-[3%] ">
+      <div className="w-[90%] h-full rounded-[6%] p-[2%] bg-[#B18423] shadow-lg ">
         <div className="bg-[#DC6E0E] rounded-[6%] w-full h-full p-[3%]">
           <div className="bg-gradient-to-b p-[2%] rounded-[6%] from-[#EFC54C] shadow-lg via-[#F98F08] to-[#943E00] w-full h-full">
             <div className="relative bg-gradient-to-br rounded-[6%] from-blue-900 to-indigo-900 w-full h-full">
@@ -29,7 +19,7 @@ const FeaturedGameCard = ({ src, setCurrentGame, setIsModalOpen }) => {
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
